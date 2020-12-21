8a6c45498a027cf73fca10b8dc590c32d7b540a5
Kazuki Hoshiyama <kxxxkhkh@gmail.com>
2020-12-21 20:09:10 +0900

    マウスカーソルが指定の要素に乗ったら色が変わり、離れたら戻る

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,20 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+window.addEventListener('load', function(){
+
+  const pullDownButton = document.getElementById("lists")
+
+  pullDownButton.addEventListener('mouseover', function(){
+    pullDownButton.setAttribute("style", "background-color:#FFBEDA;")
+    console.log("mouseover OK")
+  })
+  pullDownButton.addEventListener('mouseout', function(){
+    pullDownButton.removeAttribute("style", "background-color:#FFBEDA;")
+  })
+
+  pullDownButton.addEventListener('click', function() {
+    console.log("click OK")
+  })
+})
