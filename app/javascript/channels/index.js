4727f909597b7ef710251c768d700a1de585038b
Kazuki Hoshiyama <kxxxkhkh@gmail.com>
2020-12-21 20:22:01 +0900

    イベント発火元となった要素を取得 this

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -7,16 +7,18 @@ channels.keys().forEach(channels)
 window.addEventListener('load', function(){
 
   const pullDownButton = document.getElementById("lists")
+  const pullDownParents = document.getElementById("pull-down")
 
   pullDownButton.addEventListener('mouseover', function(){
-    pullDownButton.setAttribute("style", "background-color:#FFBEDA;")
+    this.setAttribute("style", "background-color:#FFBEDA;")
     console.log("mouseover OK")
   })
   pullDownButton.addEventListener('mouseout', function(){
-    pullDownButton.removeAttribute("style", "background-color:#FFBEDA;")
+    this.removeAttribute("style", "background-color:#FFBEDA;")
   })
 
   pullDownButton.addEventListener('click', function() {
+    pullDownParents.setAttribute("style", "display:block;")
     console.log("click OK")
   })
 })
